094e8a2f67778a19a27676c1b12c49ff25ab4c5e
martleru <68188509+martleru@users.noreply.github.com>
2024-08-01 17:00:32 +0300

    express app

---
+++ BE/app.js
@@ -0,0 +1,40 @@
+import 'express-async-errors'; 
+
+import dotenv from "dotenv";
+import express from "express";
+
+import connectDB from "./services/connect.js";
+
+import productsRouter from "./routes/products.js";
+
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+dotenv.config();
+const app = express();
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('<h1> STORE API </h1><a href="/api/products">products route</a>');
+});
+
+app.use('/api/products', productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 3001;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("Connected to MongoDB successfully");
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+  } catch (error) {
+    console.log("Failed to connect to the database:", error.message);
+  }
+};
+
+start();
