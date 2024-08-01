bfbd446c5b4424c498262a8be0f0cf55383bcef0
martleru <68188509+martleru@users.noreply.github.com>
2024-08-01 16:59:05 +0300

    MongoDB Model

---
+++ BE/models/productSchema.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Product name must be provided"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Product price must be provided"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+    // enum: ["ikea", "liddy", "caressa", "marcos"],
+  },
+});
+
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
