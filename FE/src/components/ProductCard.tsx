cb9ea1d33883fdfd78597fec7e8a507294d4cc22
martleru <68188509+martleru@users.noreply.github.com>
2024-08-21 20:33:27 +0300

    add: Added wishlist which is stored in localStorage

--- FE/src/components/ProductCard.tsx
+++ FE/src/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Product } from "@/typings/types";
 import {
   Card,
@@ -7,16 +8,55 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 type ProductCardProps = {
   product: Product;
 };
 
 const ProductCard = ({ product }: ProductCardProps) => {
+  const [liked, setLiked] = useState<boolean>(false);
+
+  useEffect(() => {
+    const likedItems = JSON.parse(localStorage.getItem("likedItems") || "[]");
+    if (likedItems.includes(product._id)) {
+      setLiked(true);
+    }
+  }, [product._id]);
+
+  const handleLiked = (id: string) => {
+    let likedItems = JSON.parse(localStorage.getItem("likedItems") || "[]");
+
+    console.log("LikedItems from handleLiked: ", likedItems)
+
+    if (likedItems.includes(id)) {
+      likedItems = likedItems.filter((itemId: string) => itemId !== id);
+      setLiked(false);
+    } else {
+      likedItems.push(id);
+      setLiked(true);
+    }
+
+    localStorage.setItem("likedItems", JSON.stringify(likedItems));
+  };
+
   return (
     <Card className="border-none flex flex-col justify-between">
       <CardHeader>
-        <CardTitle>{product.name.toUpperCase()}</CardTitle>
+        <CardTitle className="flex justify-between">
+          {product.name.toUpperCase()}
+          {liked ? (
+            <FaHeart
+              className="cursor-pointer text-red-500"
+              onClick={() => handleLiked(product._id)}
+            />
+          ) : (
+            <FaRegHeart
+              className="cursor-pointer"
+              onClick={() => handleLiked(product._id)}
+            />
+          )}
+        </CardTitle>
         <CardDescription>{product.description}</CardDescription>
       </CardHeader>
       <CardContent>
