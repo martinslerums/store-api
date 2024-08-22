6d3d31bda95d1346d3ecb153c5ea02e6612396b4
martleru <68188509+martleru@users.noreply.github.com>
2024-08-22 12:56:23 +0300

    add: Added options for CustomAPI

--- FE/src/api/api.ts
+++ FE/src/api/api.ts
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export const customApi = axios.create({
   baseURL: "http://localhost:3001/api",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    // "Authorization": "Bearer your_token_here" // Example for authentication
+  },
 });
