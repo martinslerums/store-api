99df9a59e94d34590e4beb04984fcffe2b53587b
martleru <68188509+martleru@users.noreply.github.com>
2024-08-22 12:55:45 +0300

    add: Added route for wishlist items

--- BE/routes/product.js
+++ BE/routes/product.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllProducts, getAllProductsStatic } from "../controllers/products.js";
+import { getAllProducts, getAllProductsStatic, getWishlistProducts } from "../controllers/products.js";
 
 const productRouter = express.Router();
 
 productRouter.route("/products").get(getAllProducts);
+productRouter.route("/wishlist").post(getWishlistProducts);
+
+//Testing Purposes
 productRouter.route("/static").get(getAllProductsStatic);
 
 export default productRouter;
