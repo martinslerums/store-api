b1c7bf31ca19c473c405266880b887eb8c751ae8
martleru <68188509+martleru@users.noreply.github.com>
2024-08-24 22:35:37 +0300

    feat: Fixed usage of react-router-dom

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,28 +1,50 @@
-import './index.css'
-import React from 'react'
+import "./index.css";
 
-import App from './App.tsx'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from './components/Navbar.tsx';
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProductsPage from "./pages/ProductsPage";
+import WishListPage from "./pages/WishListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/wish-list",
+        element: <WishListPage />,
+      },
+    ],
+  },
+]);
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
-    }
-  }
+    },
+  },
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <QueryClientProvider client={queryClient}>
-        <Navbar />
-        <App />
-      </QueryClientProvider> 
-    </Router>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ FE/src/router.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import ProductsPage from "./pages/ProductsPage.tsx";
+import WishListPage from "./pages/WishListPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/wish-list",
+        element: <WishListPage />,
+      },
+    ],
+  },
+]);
+
+export default router;
