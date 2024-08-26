ba8cd696224fa87a2c2e09b4868d6c7cff409e99
martleru <68188509+martleru@users.noreply.github.com>
2024-08-26 15:23:48 +0300

    update: Updated typings

--- FE/src/typings/types.ts
+++ FE/src/typings/types.ts
@@ -1,24 +1,15 @@
-export type Product = {
-  _id: string;
-  name: string;
-  price: number;
-  featured?: boolean;
-  description: string;
-  company: "ikea" | "liddy" | "caressa" | "marcos";
-  type: "sofa" | "table" | "chair";
-  rating?: number;
-  image?: string;
-  createdAt?: string;
-};
 
 export type GetProductsData = {
-  products: Product[];
+  //products: (Sofa | Product)[]; if array can contain both types.
+  data: {
+    products: Sofa [];
+  }
   nbHits: number;
 };
 
 export type GetProductFilters = {
   featured?: boolean;
-  company?: "ikea" | "liddy" | "caressa" | "marcos";
+  company?: "ikea" | "mebeles1" | "caressa" | "marcos";
   type?: "sofa" | "table" | "chair";
   name?: string;
   sort?: string;
@@ -26,4 +17,45 @@ export type GetProductFilters = {
   numericFilters?: string;
   limit?: number;
   page?: number;
+};
+
+export type GetSofaFilters = {
+  featured?: boolean;
+  company?: "ikea" | "mebeles1" | "caressa" | "marcos";
+  type?: "sofa" | "corner-sofa" | "bed";
+  name?: string;
+  color?: string
+  seat_size?: number
+  dimensions?: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  material?: "leather" | "polyester" | "velvet" | "fabric";
+  sort?: string;
+  fields?: string;
+  numericFilters?: string;
+  limit?: number;
+  page?: number;
+};
+
+export type Sofa = {
+  _id: string,
+  name: string;
+  price: number;
+  featured?: boolean;
+  description: string;
+  type: "sofa" | "corner-sofa" | "bed";
+  material: "leather" | "polyester" | "velvet" | "fabric";
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  color: string;
+  seat_size: number;
+  rating?: number;
+  company: "ikea" | "mebeles1" | "caressa" | "marcos";
+  image: string;
+  createdAt?: string;
 };
