06bc49a0bab9fcc400c7d5760dd96f4ecd318884
martleru <68188509+martleru@users.noreply.github.com>
2024-08-26 15:20:36 +0300

    add: Added hopepage and 404 page (changes to be made)

---
+++ FE/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+
+const HomePage = () => {
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        This is my HomePage 
+      </h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ FE/src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return ( 
+      <div>
+          Page Not Found component
+      </div>
+   );
+}
+
+export default NotFoundPage
