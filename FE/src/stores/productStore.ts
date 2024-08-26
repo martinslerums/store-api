c94752215ebf41e08719ff58f63c8ea257b32983
martleru <68188509+martleru@users.noreply.github.com>
2024-08-26 15:24:53 +0300

    update: Updated productStore

--- FE/src/stores/productStore.ts
+++ FE/src/stores/productStore.ts
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 
 type ProductStore = {
-  type?: "sofa" | "chair" | "table";
-  setType: (type?: "sofa" | "chair" | "table") => void;
+  type?: "sofa" | "corner-sofa" | "bed";
+  setType?: (type?: "sofa" | "corner-sofa" | "bed") => void;
   name?: string;
-  setName: (name?: string) => void;
-  company?: "ikea" | "liddy" | "caressa" | "marcos";
-  setCompany: (company?: "ikea" | "liddy" | "caressa" | "marcos") => void;
+  setName?: (name?: string) => void;
+  company?: "ikea" | "mebeles1" | "caressa" | "marcos";
+  setCompany?: (company?: "ikea" | "mebeles1" | "caressa" | "marcos") => void;
 };
 
 export const useProductStore = create<ProductStore>((set) => ({
