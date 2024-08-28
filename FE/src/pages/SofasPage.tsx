f6e56716ffcb3f94131d9073a5d53359b0465c7b
martleru <68188509+martleru@users.noreply.github.com>
2024-08-28 20:21:47 +0300

    add: Added products pages

---
+++ FE/src/pages/SofasPage.tsx
@@ -0,0 +1,59 @@
+import ProductCard from "@/components/ProductCard";
+import Filter from "@/components/Filter";
+import { useProductStore } from "@/stores/productStore";
+import { useEffect } from "react";
+import useGetProducts from "@/hooks/useGetProducts";
+import { Sofa } from "@/typings/types";
+
+const SofasPage = () => {
+
+  const { type, name } = useProductStore();
+  const filters = { type, name };
+
+  const { data, isLoading, isError } = useGetProducts<Sofa []>("sofas", filters);
+  const { products } = data || {};
+
+  console.log("Data from SofaPAGE", data)
+
+  useEffect(() => {
+    console.log("Component Mounted");
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      <div className="max-w-[300px] w-full border border-black">
+        <Filter />
+      </div>
+      <div className="flex-1 p-4">
+        {isLoading && (
+          <div className="text-lg text-gray-500 flex justify-center items-center h-screen">
+            Loading...
+          </div>
+        )}
+        {isError && (
+          <div className="text-lg text-red-500 flex justify-center items-center h-screen">
+            Error loading products
+          </div>
+        )}
+
+        {!isLoading && !isError && (
+          <div>
+            {!products || products.length === 0 ? (
+              <div className="text-lg text-gray-500 flex justify-center items-center">
+                No products available.
+              </div>
+            ) : (
+              <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+                {products.map((product: Sofa) => (
+                  <ProductCard key={product._id} product={product} />
+                ))}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SofasPage;
