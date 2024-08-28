57000bff395ad438b2220109f5f675e13368d7cf
martleru <68188509+martleru@users.noreply.github.com>
2024-08-28 20:20:05 +0300

    add: Added product - Chair route in back-end

--- BE/controllers/products.js
+++ BE/controllers/products.js
@@ -1,4 +1,5 @@
 import Sofa from "../models/sofaSchema.js";
+import Chair from "../models/chairSchema.js";
 import mongoose from "mongoose";
 
 //For testing purposes with Static data ( hard-coded )
@@ -11,80 +12,30 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, type, name, sort, fields, numericFilters } =
-    req.query;
+
+  const { name } = req.query;
 
   const queryObject = {};
 
-  if (featured) {
-    queryObject.featured = featured === "true" ? true : false;
-  }
-
-  if (company) {
-    queryObject.company = company;
-  }
-
-  if (type) {
-    queryObject.type = type;
-  }
-
   if (name) {
-    queryObject.name = { $regex: name, $options: "i" };
-  }
-
-  if (numericFilters) {
-    const operatorMap = {
-      "<=": "$lte",
-      "<": "$lt",
-      "=": "$eq",
-      ">": "$gt",
-      ">=": "$gte",
-    };
-
-    const regEx = /(<=|>=|<|>|=)/g;
-
-    let filters = numericFilters.replace(
-      regEx,
-      (match) => `-${operatorMap[match]}-`
-    );
-
-    const options = ["price", "rating"];
-    filters = filters.split(",").forEach((item) => {
-      const [field, operator, value] = item.split("-");
-
-      if (options.includes(field)) {
-        queryObject[field] = { [operator]: Number(value) };
-      }
-    });
-  }
-
-  console.log("queryObject: ", queryObject);
-
-  let result = Product.find(queryObject);
-  /* SORTING */
-  if (sort) {
-    const sortList = sort.split(",").join(" ");
-    result = result.sort(sortList);
+    queryObject.name = { $regex: name, $options: "i" }; 
   } else {
-    result = result.sort("createdAt");
+    queryObject.featured = true; 
   }
 
-  /* FIELDS ( SELECT ) */
-  if (fields) {
-    const fieldsList = fields.split(",").join(" ");
-    result = result.select(fieldsList);
-  }
+  const [sofas, chairs] = await Promise.all([
+    Sofa.find(queryObject).lean(),
+    Chair.find(queryObject).lean(),
+  ]);
 
-  /* LIMIT (products returns) SKIP (skips products for pagination) */
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
-
-  result = result.skip(skip).limit(limit);
-
-  const products = await result;
-
-  res.status(200).json({ products, nbHits: products.length });
+  res.status(200).json({
+    success: true,
+    products: { sofas, chairs },
+    nbHits: {
+      sofas: sofas.length,
+      chairs: chairs.length,
+    },
+  });
 };
 
 const getWishlistProducts = async (req, res) => {
@@ -105,7 +56,12 @@ const getWishlistProducts = async (req, res) => {
       .json({ success: false, error: "Too many productIds" });
   }
 
-  const products = await Sofa.find({ _id: { $in: productIds } }).lean();
+  const [sofas, chairs] = await Promise.all([
+    Sofa.find({ _id: { $in: productIds } }).lean(),
+    Chair.find({ _id: { $in: productIds } }).lean(),
+  ]);
+
+  const products = [...sofas, ...chairs];
 
   console.log(`Product IDs requested: ${productIds.join(", ")}`);
 
@@ -116,7 +72,7 @@ const getWishlistProducts = async (req, res) => {
 };
 
 const getAllSofas = async (req, res) => {
-  const { featured, company, type, name, sort, fields, numericFilters } =
+  const { featured, company, type, sort, fields, numericFilters } =
     req.query;
 
   const queryObject = {};
@@ -133,9 +89,6 @@ const getAllSofas = async (req, res) => {
     queryObject.type = type;
   }
 
-  if (name) {
-    queryObject.name = { $regex: name, $options: "i" };
-  }
 
   if (numericFilters) {
     const operatorMap = {
@@ -189,7 +142,89 @@ const getAllSofas = async (req, res) => {
 
   const products = await result;
 
-  res.status(200).json({ products, nbHits: products.length });
+  res.status(200).json({
+    success: true,
+    products: products,
+    nbHits: products.length,
+  });
+};
+
+const getAllChairs = async (req, res) => {
+  const { featured, company, type, sort, fields, numericFilters } =
+    req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (type) {
+    queryObject.type = type;
+  }
+
+ 
+  if (numericFilters) {
+    const operatorMap = {
+      "<=": "$lte",
+      "<": "$lt",
+      "=": "$eq",
+      ">": "$gt",
+      ">=": "$gte",
+    };
+
+    const regEx = /(<=|>=|<|>|=)/g;
+
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  console.log("queryObject in Chair Controller: ", queryObject);
+
+  let result = Chair.find(queryObject);
+  /* SORTING */
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  /* FIELDS ( SELECT ) */
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  /* LIMIT (products returns) SKIP (skips products for pagination) */
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 100;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+
+  res.status(200).json({
+    success: true,
+    products: products,
+    nbHits: products.length,
+  });
 };
 
 export {
@@ -197,4 +232,5 @@ export {
   getAllProducts,
   getWishlistProducts,
   getAllSofas,
+  getAllChairs,
 };
--- BE/routes/product.js
+++ BE/routes/product.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getAllProducts, getAllProductsStatic, getWishlistProducts, getAllSofas } from "../controllers/products.js";
+import { getAllProducts, getAllProductsStatic, getWishlistProducts, getAllSofas, getAllChairs } from "../controllers/products.js";
 
 const productRouter = express.Router();
 
 productRouter.route("/").get(getAllProducts);
 
 productRouter.route("/sofas").get(getAllSofas);
+productRouter.route("/chairs").get(getAllChairs);
 
 productRouter.route("/wishlist").post(getWishlistProducts);
 
