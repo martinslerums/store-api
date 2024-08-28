a7a48f29076454f1801034abc11af04ed945bb1f
martleru <68188509+martleru@users.noreply.github.com>
2024-08-28 20:20:59 +0300

    update: Absctracting router within single file.

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -3,35 +3,10 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import MainLayout from "./layouts/MainLayout";
-import HomePage from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFoundPage";
-import ProductsPage from "./pages/ProductsPage";
-import WishListPage from "./pages/WishListPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/products",
-        element: <ProductsPage />,
-      },
-      {
-        path: "/wishlist",
-        element: <WishListPage />,
-      },
-    ],
-  },
-]);
+import router from "./router";
 
 const queryClient = new QueryClient({
   defaultOptions: {
--- FE/src/router.tsx
+++ FE/src/router.tsx
@@ -5,6 +5,8 @@ import HomePage from "./pages/HomePage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 import ProductsPage from "./pages/ProductsPage.tsx";
 import WishListPage from "./pages/WishListPage.tsx";
+import SofasPage from "./pages/SofasPage.tsx";
+import ChairsPage from "./pages/ChairsPage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +21,14 @@ const router = createBrowserRouter([
       {
         path: "/products",
         element: <ProductsPage />,
+      },
+      {
+        path: "/products/sofas",
+        element: <SofasPage />,
+      },
+      {
+        path: "/products/chairs",
+        element: <ChairsPage />,
       },
       {
         path: "/wishlist",
