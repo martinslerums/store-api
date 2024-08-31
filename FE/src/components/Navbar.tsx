fa1b3c68288d4a426931a13e3f1e78901c15c4dd
martleru <68188509+martleru@users.noreply.github.com>
2024-08-31 22:17:26 +0300

    update: Updated Navigation bar to include searchbox and links

--- FE/src/components/Navbar.tsx
+++ FE/src/components/Navbar.tsx
@@ -3,12 +3,24 @@ import { CiHeart, CiShoppingCart } from "react-icons/ci";
 import { useEffect, useState } from "react";
 import useWishlistStore from "@/stores/wishlistStore";
 
+import SearchBox from "./SearchBox";
+import shopLogo from "../assets/logo.svg";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
 const Navbar = () => {
   const { likedItems, initializeLikedItems } = useWishlistStore();
   const [likedCount, setLikedCount] = useState(likedItems.length);
 
   useEffect(() => {
-    initializeLikedItems(); // Initialize on mount
+    initializeLikedItems();
   }, [initializeLikedItems]);
 
   useEffect(() => {
@@ -16,57 +28,143 @@ const Navbar = () => {
   }, [likedItems]);
 
   return (
-    <header className="bg-gray-800 p-4">
-      <nav className="flex items-center justify-between">
+    <header className="">
+      <div className=" bg-slate-800">
+        <div className="flex justify-between container">
+          <p className="text-white">Visit our store at Somestreet 35b</p>
+          <p className="text-white hover:text-orange-400">Contacts</p>
+        </div>
+      </div>
+      <div className="container flex justify-between py-8">
+        <div>SOCIAL MEDIA LINKS</div>
+
         <Link to="/" className="flex items-center">
-          <img
-            src="/path-to-your-logo.png"
-            alt="Logo"
-            className="h-10 w-auto"
-          />
+          <img src={shopLogo} alt="Logo" className="h-10 w-auto" />
         </Link>
-        <ul className="flex space-x-4 items-center">
-          <li>
-            <Link
-              to="/products"
-              className="text-white font-bold hover:text-orange-400"
-            >
-              Products
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/register"
-              className="text-white font-bold hover:text-orange-400"
-            >
-              Register
-            </Link>
-          </li>
-          <li className="relative">
-            <Link
-              to="/wish-list"
-              className="text-white hover:text-orange-400 text-2xl relative"
-            >
-              <CiHeart />
-              {likedCount > 0 && (
-                <span className="absolute -top-1 -right-3 bg-red-500 text-white rounded-full h-5 w-5 text-xs flex items-center justify-center">
-                  {likedCount}
-                </span>
-              )}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/cart"
-              className="text-white hover:text-orange-400 text-2xl"
-            >
-              <CiShoppingCart />
-            </Link>
-          </li>
-        </ul>
-      </nav>
+
+        <div className="flex items-center gap-4 relative">
+          <SearchBox />
+
+          <Link
+            to="/wishlist"
+            className="hover:text-orange-400 text-2xl relative"
+          >
+            <CiHeart size={30} />
+            {likedCount > 0 && (
+              <span className="absolute -top-1 -right-3 bg-red-500 rounded-full h-5 w-5 text-xs flex items-center justify-center">
+                {likedCount}
+              </span>
+            )}
+          </Link>
+
+          <Link to="/cart" className="hover:text-orange-400 text-2xl">
+            <CiShoppingCart size={30} />
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <NavigationMenu className="mx-auto">
+          
+          <NavigationMenuList className="flex space-x-4">
+
+            <NavigationMenuItem>
+
+              <NavigationMenuTrigger className="cursor-pointer px-4 py-2 rounded-lg transition duration-200 ease-in-out ">
+                Mēbeles
+              </NavigationMenuTrigger>
+
+              <NavigationMenuContent className="container flex flex-col">
+                <Link to="/products">
+                  <NavigationMenuLink className="text-gray-700 hover:underline py-1 text-lg w-full">
+                    Dīvāni
+                  </NavigationMenuLink>
+                </Link>
+                <Link to="#">
+                  <NavigationMenuLink className="text-gray-700 hover:underline py-1 text-lg">
+                    Krēsli
+                  </NavigationMenuLink>
+                </Link>
+                <Link to="#">
+                  <NavigationMenuLink className="text-gray-700 hover:underline py-1 text-lg">
+                    Gultas
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuContent>
+              
+            </NavigationMenuItem>
+
+            <NavigationMenuItem className="relative">
+              <NavigationMenuTrigger className="cursor-pointer px-4 py-2 rounded-lg transition duration-200 ease-in-out">
+                Virtuve
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="container absolute left-200 flex flex-col">
+                <Link to="#">
+                  <NavigationMenuLink className="text-gray-700 hover:underline py-1 text-lg">
+                    Documentation
+                  </NavigationMenuLink>
+                </Link>
+                <Link to="#">
+                  <NavigationMenuLink className="text-gray-700 hover:underline py-1 text-lg">
+                    Something
+                  </NavigationMenuLink>
+                </Link>
+                <Link to="#">
+                  <NavigationMenuLink className="text-gray-700 hover:underline py-1 text-lg">
+                    Something
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </header>
   );
 };
 
 export default Navbar;
+
+{
+  /* <NavigationMenuItem>
+<NavigationMenuTrigger>Virtuve</NavigationMenuTrigger>
+<NavigationMenuContent>
+  <Link to="#">
+    <NavigationMenuLink className="text-gray-700 hover:text-orange-400">
+      Documentation
+    </NavigationMenuLink>
+  </Link>
+  <Link to="#">
+    <NavigationMenuLink className="text-gray-700 hover:text-orange-400">
+      Something
+    </NavigationMenuLink>
+  </Link>
+  <Link to="#">
+    <NavigationMenuLink className="text-gray-700 hover:text-orange-400">
+      Something
+    </NavigationMenuLink>
+  </Link>
+</NavigationMenuContent>
+</NavigationMenuItem>
+<NavigationMenuItem>
+<NavigationMenuTrigger>Dārzs</NavigationMenuTrigger>
+<NavigationMenuContent>
+  <Link to="#">
+    <NavigationMenuLink className="text-gray-700 hover:text-orange-400">
+      Documentation
+    </NavigationMenuLink>
+  </Link>
+  <Link to="#">
+    <NavigationMenuLink className="text-gray-700 hover:text-orange-400">
+      Something
+    </NavigationMenuLink>
+  </Link>
+  <Link to="#">
+    <NavigationMenuLink className="text-gray-700 hover:text-orange-400">
+      Something
+    </NavigationMenuLink>
+  </Link>
+</NavigationMenuContent>
+</NavigationMenuItem> */
+}
