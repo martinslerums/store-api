596992920d4c4362a422b6f3093e28a6937b614b
martleru <68188509+martleru@users.noreply.github.com>
2024-09-02 16:11:07 +0300

    add: Added values to zustand (products) store

--- FE/src/stores/productStore.ts
+++ FE/src/stores/productStore.ts
@@ -1,19 +1,28 @@
+import { AllCompanies, AllMaterials, AllTypes } from "@/typings/types";
 import { create } from "zustand";
 
 type ProductStore = {
-  type?: "sofa" | "corner-sofa" | "bed";
-  setType?: (type?: "sofa" | "corner-sofa" | "bed") => void;
   name?: string;
-  setName?: (name?: string) => void;
-  company?: "ikea" | "mebeles1" | "caressa" | "marcos";
-  setCompany?: (company?: "ikea" | "mebeles1" | "caressa" | "marcos") => void;
+  setName: (name?: string) => void;
+  type?: AllTypes;
+  setType: (type?: AllTypes) => void;
+  company?: AllCompanies;
+  setCompany: (company?: AllCompanies) => void;
+  material?: AllMaterials;
+  setMaterial: (material?: AllMaterials) => void;
+  color?: string;
+  setColor: (color?: string) => void;
 };
 
 export const useProductStore = create<ProductStore>((set) => ({
-  type: undefined,
-  setType: (type) => set({ type }),
   name: undefined,
   setName: (name) => set({ name }),
+  type: undefined,
+  setType: (type) => set({ type }),
   company: undefined,
   setCompany: (company) => set({ company }),
+  material: undefined,
+  setMaterial: (material) => set({ material }),
+  color: undefined,
+  setColor: (color) => set({ color }),
 }));
