812cb530911f2cc0f31d1271dca53e4c631c1641
martleru <68188509+martleru@users.noreply.github.com>
2024-09-02 16:09:21 +0300

    add: Filters component.
    Component in sync with zustand store so we can filter products

--- FE/src/components/Filter.tsx
+++ FE/src/components/Filter.tsx
@@ -1,99 +1,62 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import { useState } from "react";
-
-import { FaSearch } from "react-icons/fa";
-
+import useGetFilters from "@/hooks/useGetFilters";
+import FilterSection from "./FilterSection";
+import { useProductStore } from "@/stores/productStore";
+import { AllCompanies, AllMaterials, AllTypes } from "@/typings/types";
 
 const Filter = () => {
+  const { data } = useGetFilters();
+  const { uniqueCompanies, uniqueColors, uniqueMaterials, uniqueTypes } =
+    data || {};
 
-  const productTypes = [
-    { name: "All products", searchQuery: "" },
-    { name: "Sofas", searchQuery: "sofa" },
-    { name: "Chairs", searchQuery: "chair" },
-    { name: "Tables", searchQuery: "table" },
-  ];
+  const {
+    company,
+    setCompany,
+    material,
+    setMaterial,
+    color,
+    setColor,
+    type,
+    setType,
+  } = useProductStore();
 
-  const handleProductType = (type: string) => {
-    const currentUrl = new URL(window.location.href);
+  const filters = [company, material, color, type]
 
-    if (type) {
-      currentUrl.searchParams.set("type", type);
-    } else {
-      currentUrl.searchParams.delete("type");
-    }
-
-    window.history.pushState({}, "", currentUrl);
-    window.dispatchEvent(new PopStateEvent("popstate"));
-  };
-
-  
-  const [productName, setProductName] = useState("")
-
-  const handleProductNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setProductName(event.target.value);
-  };
-
-  const handleProductName = (name: string) => {
-    const currentUrl = new URL(window.location.href);
-
-    if (name) {
-      currentUrl.searchParams.set("name", name);
-    } else {
-      currentUrl.searchParams.delete("name");
-    }
-
-    // Update browser's URL without reloading
-    window.history.pushState({}, "", currentUrl);
-    
-    // Dispatch a popstate event to notify the application of the URL change
-    window.dispatchEvent(new PopStateEvent("popstate"));
-  };
-
-
+  console.table(filters);
 
   return (
-    <div className="border-orange-900 border h-full w-full p-4">
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Products</AccordionTrigger>
-          <AccordionContent className="flex flex-col gap-2">
-            {productTypes.map((type) => (
-              <Button
-                key={type.name}
-                variant="link"
-                onClick={() => handleProductType(type.searchQuery)}
-              >
-                <span className="text-left w-full">{type.name}</span>
-              </Button>
-            ))}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <br />
-      <div className="flex">
-        <Input
-          type="text"
-          placeholder="Find a product"
-          value={productName}
-          onChange={handleProductNameChange} 
+    <div className="h-full w-full p-4">
+      {uniqueCompanies && (
+        <FilterSection<AllCompanies>
+          title="Ražotājs"
+          items={uniqueCompanies as AllCompanies[]}
+          selectedItem={company}
+          onSelect={setCompany}
         />
-        <Button
-          className="-ml-10"
-          variant="ghost"
-          type="button"
-          size="icon"
-          onClick={() => handleProductName(productName)}
-        >
-          <FaSearch />
-        </Button>
-      </div>
+      )}
+      {uniqueTypes && (
+        <FilterSection<AllTypes>
+          title="Tips"
+          items={uniqueTypes as AllTypes[]}
+          selectedItem={type}
+          onSelect={setType}
+        />
+      )}
+      {uniqueMaterials && (
+        <FilterSection<AllMaterials>
+          title="Materiāls"
+          items={uniqueMaterials as AllMaterials[]}
+          selectedItem={material}
+          onSelect={setMaterial}
+        />
+      )}
+      {uniqueColors && (
+        <FilterSection<string>
+          title="Krāsa"
+          items={uniqueColors}
+          selectedItem={color}
+          onSelect={setColor}
+        />
+      )}
     </div>
   );
 };
