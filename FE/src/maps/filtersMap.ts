92156d5476f062e0834942425156d9612c8984e6
martleru <68188509+martleru@users.noreply.github.com>
2024-09-10 12:45:20 +0300

    add: Added Object (map) to translate raw values from database

---
+++ FE/src/maps/filtersMap.ts
@@ -0,0 +1,34 @@
+export const filtersMap: Record<string | number, string> = {
+  // Product Manufacturers
+  "ikea" : "IKEA",
+  "mebeles1" : "Mēbeles1",
+  "berry" : "Berry Mēbeles",
+
+  // Product Types
+  "corner-sofa" : "Stūra dīvāni",
+  "sofa": "Dīvāni",
+  "office-chair": "Ofisa krēsli",
+  "bar-chair": "Bāra krēsli",
+  "lounge-chair": "Atpūtas krēsli",
+
+  // Product Materials
+  "fabric": "Audums",
+  "leather": "Āda",
+  "polyester": "Poliesters",
+  "metal": "Metāls",
+  "wood": "Kokmateriāls",
+
+  // Product Colors
+  "black": "Melna",
+  "brown": "Brūna",
+  "gray": "Pelēka",
+  "darkgray": "Tumši pelēka",
+  "light gray": "Gaiši pelēka",
+  "white": "Balta",
+  "yellow": "Dzeltena",
+  "sand": "Smilškrāsa",
+  "blue": "Zila",
+  "dark blue": "Tumši zila"
+  
+
+}
