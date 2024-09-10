c11c3db76fb594ac53acd71698520b5b79825972
martleru <68188509+martleru@users.noreply.github.com>
2024-09-10 12:46:27 +0300

    feat: searchBox now sets state in URL with useProductsFilters hook.

--- FE/src/components/SearchBox.tsx
+++ FE/src/components/SearchBox.tsx
@@ -3,13 +3,15 @@ import { IoIosClose } from "react-icons/io";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useState } from "react";
-import { useProductStore } from "@/stores/productStore";
 import { useNavigate } from "react-router-dom";
+import useProductFilters from "@/hooks/useProductFilters";
 
 const SearchBox = () => {
-  const { name, setName } = useProductStore();
-  const [searchTerm, setSearchTerm] = useState(name || "");
+  const { setFilters } = useProductFilters();
+
+  const [searchTerm, setSearchTerm] = useState("");
   const [showSearch, setShowSearch] = useState(false);
+
   const navigate = useNavigate();
 
   const handleShowSearch = () => {
@@ -18,10 +20,12 @@ const SearchBox = () => {
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      if (setName) {
-        setName(searchTerm);
-      }
-      navigate("/products");
+      setFilters({ name: searchTerm });
+      const newUrl = searchTerm
+        ? `/products?name=${encodeURIComponent(searchTerm)}`
+        : `/products`;
+
+      navigate(newUrl);
     }
   };
 
@@ -29,7 +33,7 @@ const SearchBox = () => {
     <div className="relative flex items-center">
       {showSearch ? (
         <Input
-          className={`min-w-60 absolute right-full -mr-10 transform outline-none focus-visible:ring-0 focus-visible:ring-offset-0 border-l-0 border-r-0 border-t-0 border-b-2 border-black rounded-none opacity-100 tracking-wider pb-2`}
+          className="min-w-60 absolute right-full -mr-10 transform outline-none focus-visible:ring-0 focus-visible:ring-offset-0 border-l-0 border-r-0 border-t-0 border-b-2 border-black rounded-none opacity-100 tracking-wider pb-2"
           type="text"
           placeholder="Find a product"
           value={searchTerm}
