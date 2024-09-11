cf11b4f8117ed83bc5ec08b515d01798ad292b27
martleru <68188509+martleru@users.noreply.github.com>
2024-09-11 20:03:03 +0300

    update: Hook now can set multiple search params(values) for same key

--- FE/src/hooks/useProductFilters.ts
+++ FE/src/hooks/useProductFilters.ts
@@ -5,23 +5,33 @@ import { useSearchParams } from "react-router-dom";
 const useProductFilters = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const getCommaSeparatedValues = (key: string): string[] => {
+    const value = searchParams.get(key);
+    return value ? value.split(",").map((v) => v.trim()) : [];
+  };
+
   const filters: ProductFilters = {
     name: searchParams.get("name") as ProductFilters["name"],
-    company: searchParams.get("company") as ProductFilters["company"],
-    color: searchParams.get("color") as ProductFilters["color"],
-    material: searchParams.get("material") as ProductFilters["material"],
-    type: searchParams.get("type") as ProductFilters["type"],
+    company: getCommaSeparatedValues("company") as ProductFilters["company"],
+    color: getCommaSeparatedValues("color") as ProductFilters["color"],
+    material: getCommaSeparatedValues("material") as ProductFilters["material"],
+    type: getCommaSeparatedValues("type") as ProductFilters["type"],
   };
 
   const setFilters = useCallback(
     (filters: Partial<ProductFilters>) => {
       setSearchParams((params) => {
+        Object.keys(filters).forEach((key) => {
+          params.delete(key);
+        });
 
         Object.entries(filters).forEach(([key, value]) => {
-          if (value !== undefined && value !== null) {
-            params.set(key, String(value)); 
-          } else {
-            params.delete(key);
+          if (Array.isArray(value)) {
+            if (value.length > 0) {
+              params.set(key, value.join(","));
+            }
+          } else if (value !== undefined && value !== null) {
+            params.set(key, String(value));
           }
         });
 
