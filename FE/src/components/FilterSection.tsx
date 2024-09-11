3ba8a0759ac7ee3bb3706188f21a2a1230634315
martleru <68188509+martleru@users.noreply.github.com>
2024-09-11 20:03:30 +0300

    add: Multiple values can be selected for same search key

--- FE/src/components/FilterSection.tsx
+++ FE/src/components/FilterSection.tsx
@@ -1,47 +1,61 @@
 import { Checkbox } from "@/components/ui/checkbox";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
-
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { filtersMap } from "@/maps/filtersMap";
-
-
+import { useEffect } from "react";
 
 type FilterSectionProps<T extends string | number> = {
   title: string;
   items: T[];
-  selectedItem: T | undefined;
-  onSelect: (item: T | undefined) => void;
+  selectedOptions: T[];
+  onSelect: (item: T, isChecked: boolean) => void;
 };
 
 const FilterSection = <T extends string | number>({
   title,
   items,
-  selectedItem,
+  selectedOptions,
   onSelect,
-}: FilterSectionProps<T>) => (
-  <Accordion type="single" collapsible className="shadow-sm p-2">
-    <AccordionItem className="border-b-0" value={title}>
-      <AccordionTrigger>{title}</AccordionTrigger>
-      <AccordionContent className="flex flex-col gap-2">
-        {items.map((item) => (
-          <div key={item} className="flex items-center">
-            <label
-              htmlFor={`checkbox-${item}`}
-              className="flex items-center cursor-pointer"
-            >
-              <Checkbox 
-                hideCheck
-                className="h-3 w-3 rounded-full"
-                id={`checkbox-${item}`}
-                checked={selectedItem === item}
-                onCheckedChange={(checked) => onSelect(checked ? item : undefined)}
-              />
-              <span className="ml-3 font-semibold">{filtersMap[item] || item}</span>
-            </label>
-          </div>
-        ))}
-      </AccordionContent>
-    </AccordionItem>
-  </Accordion>
-);
+}: FilterSectionProps<T>) => {
+
+  useEffect(() => {
+    console.log("Component Mounted")
+  }, [])
+
+  return (
+    <Accordion type="single" collapsible className="shadow-sm p-2">
+      <AccordionItem className="border-b-0" value={title}>
+        <AccordionTrigger>{title}</AccordionTrigger>
+        <AccordionContent className="flex flex-col gap-2">
+          {items.map((item) => (
+            <div key={item} className="flex items-center">
+              <label
+                htmlFor={`checkbox-${item}`}
+                className="flex items-center cursor-pointer"
+              >
+                <Checkbox
+                  hideCheck
+                  className="h-3 w-3 rounded-full"
+                  id={`checkbox-${item}`}
+                  checked={selectedOptions.includes(item)}
+                  onCheckedChange={(checked: boolean) =>
+                    onSelect(item, checked)
+                  }
+                />
+                <span className="ml-3 font-semibold">
+                  {filtersMap[item] || item}
+                </span>
+              </label>
+            </div>
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
 
 export default FilterSection;
