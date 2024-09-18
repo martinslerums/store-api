d275168c4ded84089294e71e571fc926729c44d5
martleru <68188509+martleru@users.noreply.github.com>
2024-09-18 18:01:48 +0300

    add: Updated 404 page added return btn

--- FE/src/pages/NotFoundPage.tsx
+++ FE/src/pages/NotFoundPage.tsx
@@ -1,9 +1,24 @@
-const NotFoundPage = () => {
-  return ( 
-      <div>
-          Page Not Found component
-      </div>
-   );
-}
+import { Link } from "react-router-dom";
+import { IoReturnDownBack } from "react-icons/io5";
 
-export default NotFoundPage
+const NotFoundPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-red-600 mb-4">404</h1>
+        <p className="text-2xl font-medium text-gray-800 mb-6">
+          Page Not Found
+        </p>
+        <Link
+          to="/"
+          className="text-blue-500 hover:text-blue-700 flex items-center justify-center space-x-2"
+        >
+          <IoReturnDownBack />
+          <span> Go to Home</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
